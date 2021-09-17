102eae1eedf3db514b846cb258d390d1b46ae054
ve7gel <o31254G!>
2021-09-17 12:57:29 -0700

    replacing isy bundle

--- bundles/org.openhab.binding.isy/src/main/java/org/openhab/binding/isy/handler/IsyThingHandler.java
+++
@@ -1,6 +0,0 @@
-package org.openhab.binding.isy.handler;
-
-public interface IsyThingHandler {
-
-    public void handleUpdate(String control, String action, String node);
-}
--- bundles/org.openhab.binding.isy/src/main/java/org/openhab/binding/isy/internal/protocol/SubscriptionResponse.java
+++
@@ -1,8 +0,0 @@
-package org.openhab.binding.isy.internal.protocol;
-
-import com.thoughtworks.xstream.annotations.XStreamAlias;
-
-@XStreamAlias("SubscriptionResponse")
-public class SubscriptionResponse {
-
-}
